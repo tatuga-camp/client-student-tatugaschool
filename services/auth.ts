f7d63a9e59e5500804ea9a5f8b21f68bc0b83d6c
permlap-phola <permlap@tatugacamp.com>
2024-12-02 14:30:01 +0700

    complete add service (#2)

---
+++ services/auth.ts
@@ -0,0 +1,53 @@
+import createAxiosInstance from "./apiService";
+
+const axiosInstance = createAxiosInstance();
+
+type RequestStudentRefetchTokenService = {
+  refreshToken: string;
+};
+
+type ResponseStudentRefetchTokenService = {
+  accessToken: string;
+};
+
+export async function StudentRefetchTokenService(
+  input: RequestStudentRefetchTokenService
+): Promise<ResponseStudentRefetchTokenService> {
+  try {
+    const response = await axiosInstance({
+      method: "POST",
+      url: "/v1/auth/student/refetch-token",
+      data: { ...input },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Student Refetch Token request failed:", error.response.data);
+    throw error?.response?.data;
+  }
+}
+
+type RequestStudentSignInService = {
+  studentId: string;
+  password?: string;
+};
+
+type ResponseStudentSignInService = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export async function StudentSignInService(
+  input: RequestStudentSignInService
+): Promise<ResponseStudentSignInService> {
+  try {
+    const response = await axiosInstance({
+      method: "POST",
+      url: "/v1/auth/student/sign-in",
+      data: { ...input },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Student Sign-In request failed:", error.response.data);
+    throw error?.response?.data;
+  }
+}
---
+++ services/student.ts
@@ -0,0 +1,61 @@
+import {
+  Student,
+  StudentOnSubject,
+  Subject,
+  TeacherOnSubject,
+} from "../interfaces";
+import createAxiosInstance from "./apiService";
+
+const axiosInstance = createAxiosInstance();
+
+type RequestGetStudentService = {
+  studentId: string;
+};
+
+type ResponseGetStudentService = Student;
+export async function GetStudentService(
+  input: RequestGetStudentService
+): Promise<ResponseGetStudentService> {
+  try {
+    const response = await axiosInstance({
+      method: "GET",
+      url: `v1/students/student/${input.studentId}`,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("GetStudent request failed:", error.response.data);
+    throw error?.response?.data;
+  }
+}
+
+type RequestUpdateStudentService = {
+  query: {
+    studentId: string;
+  };
+  body: {
+    title?: string;
+    firstName?: string;
+    lastName?: string;
+    photo?: string;
+    blurHash?: string;
+    number?: string;
+    password?: string;
+  };
+};
+
+type ResponseUpdateStudentService = Student;
+export async function UpdateStudentService(
+  input: RequestUpdateStudentService
+): Promise<ResponseUpdateStudentService> {
+  try {
+    const response = await axiosInstance({
+      method: "PATCH",
+      url: `v1/students/student`,
+      data: { ...input },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("UpdateStudent request failed:", error.response.data);
+    throw error?.response?.data;
+  }
+}
