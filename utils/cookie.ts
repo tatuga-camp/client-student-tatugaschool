263ec53c6d7f1d932f63faf63e2b8704ff5bbec7
permlap-phola <permlap@tatugacamp.com>
2025-02-08 11:39:23 +0700

    rename access_token solve conflic (#21)

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -5,18 +5,18 @@ export function getRefetchtoken(ctx?: GetServerSidePropsContext): {
   refresh_token: string | null;
 } {
   const cookies = parseCookies(ctx);
-  const refresh_token = cookies.refresh_token;
+  const refresh_token = cookies.student_refresh_token;
   return { refresh_token };
 }
 
 export function getAccessToken(): { access_token: string | null } {
   const cookies = parseCookies();
-  const access_token = cookies.access_token;
+  const access_token = cookies.student_access_token;
   return { access_token };
 }
 
 export function setAccessToken({ access_token }: { access_token: string }) {
-  setCookie(null, "access_token", access_token, {
+  setCookie(null, "student_access_token", access_token, {
     path: "/",
     maxAge: 5 * 24 * 60 * 60,
   });
@@ -24,7 +24,7 @@ export function setAccessToken({ access_token }: { access_token: string }) {
 }
 
 export function setRefreshToken({ refresh_token }: { refresh_token: string }) {
-  setCookie(null, "refresh_token", refresh_token, {
+  setCookie(null, "student_refresh_token", refresh_token, {
     path: "/",
     maxAge: 5 * 24 * 60 * 60,
   });
@@ -33,9 +33,9 @@ export function setRefreshToken({ refresh_token }: { refresh_token: string }) {
 }
 
 export function removeAccessToken() {
-  destroyCookie(null, "access_token");
+  destroyCookie(null, "student_access_token");
 }
 
 export function removeRefreshToken() {
-  destroyCookie(null, "refresh_token");
+  destroyCookie(null, "student_refresh_token");
 }
